64934bcdd40afcf797bd10eecc8ff128fd8996d4
Landile-Mdlalo <ST10512643@myemeris.edu.za>
2026-03-05 12:56:08 +0200

    add comments

--- src/main/java/SimpleCalculations.java
+++ src/main/java/SimpleCalculations.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
+class  Landile, ST10512643, SimpleCalculations
 package com.mycompany.simplecalculations;
 
-/**
- *
- * @author lab_services_student
- */
+
 public class SimpleCalculations {
 
     public static void main(String[] args) {
